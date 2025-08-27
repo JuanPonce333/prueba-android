9f05dad710122af554bcf757b3649c306d55f614
JuanPonce333 <pipeponce334@gmail.com>
2025-08-27 10:27:18 -0500

    Segundo commit

--- app/src/main/java/com/example/citas2/LoginActivity.kt
+++ app/src/main/java/com/example/citas2/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.citas2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
